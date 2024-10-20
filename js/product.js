44d552336269411773bdd7ac51a8d8443a8f5807
faezehdev <faezeh0078@gmail.com>
2024-10-20 13:39:50 +0330

    complete product age

--- js/product.js
+++ js/product.js
@@ -1,11 +1,4 @@
 // Main swiper
-let IS1 = true
-let IS2 = true
-let IS3 = true
-let IS4 = true
-let IS5 = true
-let IS6 = true
-let IS7 = true
 header.classList.add('dark')
 let activeItem = document.querySelector('.activeItem')
 let fixNav = document.querySelector('.HoverNav')
@@ -36,14 +29,14 @@ const swiper = new Swiper('.swiper-Container', {
         sensitivity: 4,
     },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".Nav .swiper-pagination",
         type: "progressbar"
       },
       parallax : true,
     direction: getDirection(),
     on:{
        init(e){
-        const Slides = document.querySelectorAll('.swiper-slide-visible');
+        const Slides = document.querySelectorAll('.Pin-Wrapp > .mainSlide.swiper-slide-visible');
     for (let i = 0; i < Slides.length; i++) {
         Slides[i].classList.add('inner-visible')
     }
@@ -51,13 +44,14 @@ const swiper = new Swiper('.swiper-Container', {
 
     setTransition(e){
     let elem = document.querySelector('.roundTxt');
-    const Slides = document.querySelectorAll('.swiper-slide-visible');
+    const Slides = document.querySelectorAll('.Pin-Wrapp > .mainSlide.swiper-slide-visible');
     for (let i = 0; i < Slides.length; i++) {
         Slides[i].classList.add('inner-visible')
         let value =swiper.translate * 0.08;
-        console.log(value);
-        elem.style.transform = `translatex(80%) translatey(0%) rotate(${value}deg)`;     
+        elem.style.transform = `translatex(80%) translatey(0%) rotate(${value}deg)`;   
     }
+
+    
     },
 
        slideChange(t){
@@ -69,71 +63,97 @@ const swiper = new Swiper('.swiper-Container', {
         let id = GoSec.getAttribute('data-index')
         all.forEach(a=>{
           a.classList.remove('active')
+          a.classList.remove('DarkActive')
       })
         GoSec.classList.add('active')
         t.slides[swiper.activeIndex].classList.add('Is-active')    
 
         switch(Hash){
           case '0':{
-            //   console.log('s',0);
+              console.log('s',0);
+              GoSec.classList.add('active')
+              t.slides[swiper.activeIndex].classList.add('Is-active')  
+              document.querySelector('.Nav .Top .activeItem').classList.remove('DarkActive')
+              header.classList.add('dark')
               break
           }
           case '1':{
-            //   console.log('s',1);
-              if(IS1){
-             
-                IS1 =false
-            }
+              console.log('s',1);
+              GoSec.classList.add('DarkActive')
+              t.slides[swiper.activeIndex].classList.add('Is-active')  
+              document.querySelector('.Nav .Top .activeItem').classList.add('DarkActive')
+              header.classList.remove('dark')
               break
           }
           case '2':{
-            //   console.log('s',2);
-            
-                  IS2 =false
-            
-             
+              console.log('s',2);
+              GoSec.classList.add('active')
+              t.slides[swiper.activeIndex].classList.add('Is-active')  
+              document.querySelector('.Nav .Top .activeItem').classList.remove('DarkActive')
+              header.classList.add('dark')
               break
           }
-          case '3':{
-            //   console.log('s',3);
-           
-                  IS3 =false
-              
-              break
-          }
-          case '4':{
-          
-                IS4 =false
-            
-              break
-          }
-          case '5':{
-            //   console.log('s',5);
-            
-                  IS5 =false
-              
-              break
-          }
-          case '6':{
-            // console.log('s',6);
-                
-             IS6 =false
-            
-            break
         }
-        case '7':{
-          console.log('s',7);
-               
-              IS7 =false
-          
-          break
-      }
-        }
-
-                    },
-      
-                }
+          },     
+          }
   });
   function getDirection() {
     return window.innerWidth > 767 ? 'horizontal' : 'vertical';
 }
+let Lab = document.querySelector('.Nav .Top')
+let HoverNav = document.querySelector('.HoverNav')
+let NavItems = document.querySelectorAll('.HoverNav .NItem')
+NavItems.forEach((n)=>{
+
+    n.addEventListener('click',(e)=>{
+      NavItems.forEach(l=>{
+        l.classList.remove('active')
+      })
+      e.currentTarget.classList.add('active')
+
+      let Hash = e.target.getAttribute('data-index')
+      let GoSec = document.querySelector(`[data-index='${Hash}']`)
+      let id = GoSec.getAttribute('data-index')
+      console.log('h',Hash);
+      console.log('sec',GoSec);
+
+      swiper.slideTo(id, 100)
+  
+    })
+
+
+})
+// gallery slider
+let SwiperBanner= new Swiper ('.swiper-gallery', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  effect:'fade' ,
+  fadeEffect: {
+    crossFade: true,
+  },
+ speed:2000,
+ pagination: {
+  el: '.gallery-Container .swiper-pagination',
+        clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">'+ '</span>';
+    },
+},
+navigation: {
+  nextEl: ".gallery-Container .swiper-button-next",
+  prevEl: ".gallery-Container .swiper-button-prev",
+}
+  })
+
+  // product slider
+let SwiperPro= new Swiper ('.swiper-product', {
+  direction:'vertical',
+  slidesPerView: 1,
+  spaceBetween: 0,
+  effect:'slide' ,
+ speed:200,
+navigation: {
+  nextEl: ".Related-Container .swiper-button-next",
+  prevEl: ".Related-Container .swiper-button-prev",
+}
+  })
